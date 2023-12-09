f0b9ad338d675e2205afad739fd95b3726dca224
Vitor Savian <vitorandresaviandev@gmail.com>
2023-12-09 11:07:39 -0300

    Initial commit

---
+++ longroad/longroad.cpp
@@ -0,0 +1,13 @@
+﻿// longroad.cpp : Defines the entry point for the application.
+//
+
+#include "longroad.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "TESTING SOME STUFF" << endl;
+	return 0;
+}
