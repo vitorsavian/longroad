617efa7b01245f31c8e5cf769ef7b47db5ec2b25
Vitor Savian <vitor.savian@suse.com>
2026-01-13 21:39:48 -0300

    chore: finished texture chapter, now i need to do the exercises lol

--- src/main.cpp
+++ src/main.cpp
@@ -114,9 +114,10 @@ int main() {
   // uncomment this call to draw in wireframe polygons.
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
+  unsigned int textures[2];
+  glGenTextures(2, textures);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
   // set the texture wrapping/filtering options (on currently bound texture)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -132,10 +133,31 @@ int main() {
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
   } else {
-    std::cout << "Failed to load texture" << std::endl;
+    std::cout << "Failed to load texture container.jpg" << std::endl;
   }
 
   stbi_image_free(data);
+
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char *data2 =
+      stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+  if (data2) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, data2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture awesomeface.png" << std::endl;
+  }
+
+  stbi_image_free(data2);
+
+  // remove this from the while loop
+  ourShader.use();
+  glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+  ourShader.setInt("texture2", 1);
 
   // render loopf
   // -----------
@@ -149,7 +171,6 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    ourShader.use();
     /*ourShader.setFloat("offset", 0.3f);*/
 
     /*float timeValue = glfwGetTime();
@@ -164,7 +185,6 @@ int main() {
     // there's no need to bind it every time, but we'll do so to keep things a
     // bit more organized
 
-    glBindTexture(GL_TEXTURE_2D, texture);
     glBindVertexArray(VAOs[0]);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
